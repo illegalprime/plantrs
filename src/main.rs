e638daad7190be30e4b9f9f73fb0560e2a4f0497
Michael Eden <themichaeleden@gmail.com>
2024-01-06 16:53:50 -0500

    blink a gpio

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-fn main() {
+use esp_idf_svc::hal::delay::FreeRtos;
+use esp_idf_svc::hal::gpio::PinDriver;
+use esp_idf_svc::hal::peripherals::Peripherals;
+use esp_idf_svc::sys::EspError;
+
+fn main() -> Result<(), EspError> {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_svc::sys::link_patches();
@@ -7,4 +12,17 @@ fn main() {
     esp_idf_svc::log::EspLogger::initialize_default();
 
     log::info!("Hello, world!");
+
+    // grab a pin, make it an output
+    let gpio10 = Peripherals::take()?.pins.gpio10;
+    let mut pin = PinDriver::output(gpio10)?;
+
+    // twiddle
+    loop {
+        pin.set_high()?;
+        FreeRtos::delay_ms(500);
+
+        pin.set_low()?;
+        FreeRtos::delay_ms(500);
+    }
 }
