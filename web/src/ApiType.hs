8439f29e413fb994002e87d1a29ef51e033d8948
Michael Eden <themichaeleden@gmail.com>
2024-01-07 21:46:30 -0500

    haskell server to bridge mqtt

---
+++ web/src/ApiType.hs
@@ -0,0 +1,105 @@
+module ApiType (
+  app,
+) where
+
+import Control.Concurrent (Chan, readChan)
+import Control.Monad.Loops (untilJust)
+import Data.Aeson
+import Data.Aeson.Casing (snakeCase)
+import Network.MQTT.Client (MQTTClient, QoS (QoS1), SubOptions (_subQoS), publish, subOptions, subscribe)
+import Servant
+import System.Random (randomIO)
+import Text.Printf (printf)
+
+responseTopic :: Text
+responseTopic = "plantrs/responses"
+
+type Plant = Capture "plant" Text
+
+type WaterAPI = "water" :> QueryParam "t" Word32 :> Post '[JSON] Response
+
+type AddAPI = "add" :> Capture "a" Word32 :> Capture "b" Word32 :> Post '[JSON] Response
+
+type Api =
+  Plant :> WaterAPI
+    :<|> Plant :> AddAPI
+
+appApi :: Proxy Api
+appApi = Proxy
+
+waterHandler :: Commander -> Text -> Maybe Word32 -> Handler Response
+waterHandler commander plant secs = do
+  liftIO $ commander plant $ Drive $ fromMaybe 0 secs
+
+addHandler :: Commander -> Text -> Word32 -> Word32 -> Handler Response
+addHandler commander plant a b = do
+  liftIO $ commander plant $ Add a b
+
+server :: Commander -> Server Api
+server cmd = waterHandler cmd :<|> addHandler cmd
+
+type Commander = Text -> Command -> IO Response
+
+runCommand :: MQTTClient -> Chan LByteString -> Commander
+runCommand mc msgs plant cmd = do
+  -- prepare request
+  nonce <- randomIO -- TODO: best way?
+  let request = Request cmd responseTopic nonce
+  let topic = fromString $ printf "plantrs/%s/request" plant
+  let encoded = encode request
+  putLBSLn $ "sending request: " <> encoded
+  -- send it
+  publish mc topic encoded False
+  -- wait for a response
+  untilJust $ do
+    msg <- readChan msgs -- TODO: semantics of multiple consumers?
+    putLBSLn $ "received message: " <> msg
+    case decode msg of
+      Nothing -> Nothing <$ putLBSLn ("could not parse: " <> msg)
+      Just r@Response {correlate = c} | c == nonce -> pure $ Just r
+      _ -> pure Nothing
+
+app :: MQTTClient -> Chan LByteString -> IO Application
+app mc msgs = do
+  -- init command handler
+  let commander = runCommand mc msgs
+  -- subscribe to command response topic
+  print =<< subscribe mc [(topic, options)] []
+  -- build app description
+  pure $ serve appApi $ server commander
+  where
+    topic = fromString . toString $ responseTopic
+    options = subOptions {_subQoS = QoS1}
+
+data Command
+  = Add Word32 Word32
+  | Drive Word32
+  deriving stock (Eq, Show, Generic)
+
+instance ToJSON Command where
+  toJSON = genericToJSON snakeCaseJson
+
+data Request = Request
+  { command :: Command
+  , response_topic :: Text
+  , correlate :: Word64
+  }
+  deriving stock (Eq, Show, Generic)
+
+instance ToJSON Request
+
+data Response = Response
+  { body :: Text
+  , correlate :: Word64
+  }
+  deriving stock (Eq, Show, Generic)
+
+instance ToJSON Response
+instance FromJSON Response
+
+snakeCaseJson :: Options
+snakeCaseJson =
+  defaultOptions
+    { sumEncoding = ObjectWithSingleField
+    , constructorTagModifier = snakeCase
+    }
--- web/src/Main.hs
+++ web/src/Main.hs
@@ -1,14 +1,19 @@
 module Main where
 
-import Main.Utf8 qualified as Utf8
+import ApiType (app)
+import Control.Concurrent (newChan, writeChan)
+import Data.Maybe (fromJust)
+import Network.MQTT.Client
+import Network.URI (parseURI)
+import Network.Wai.Handler.Warp
 
-{- |
- Main entry point.
-
- The `, run` script will invoke this function.
--}
 main :: IO ()
 main = do
-  -- For withUtf8, see https://serokell.io/blog/haskell-with-utf8
-  Utf8.withUtf8 $ do
-    putTextLn "Hello 🌎"
+  let uri = fromJust $ parseURI "mqtt://10.10.0.184"
+  putTextLn "Running server on port 8081..."
+  msgs <- newChan
+  mc <- connectURI mqttConfig {_msgCB = SimpleCallback $ msgReceived msgs} uri
+  api <- app mc msgs
+  run 8081 api
+  where
+    msgReceived chan _ _ m _ = writeChan chan m
